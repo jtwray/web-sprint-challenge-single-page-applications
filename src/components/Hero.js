7ed85fbaf0da1a1ae8f11eb040b011384119d4c4
tucker <tuckerwray84@gmail.com>
2020-07-24 12:14:03 -0400

    IMplements a basic architecture for the pizza ordering system--setup integration testing with cypress. add scfipts for cypress to the package.json scripts object- implements a hero component to display the large panned pizza image available in the ./Assets directory. Base Application architecture implemented

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <img
+      src="../../Assets/Pizza.jpg"
+      alt="A large pizza with thin crust and all the toppings!"
+    />
+  );
+};
