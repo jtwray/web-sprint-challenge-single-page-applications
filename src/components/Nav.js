4e0183cc318941225ffea1cac2b8db0a375cd786
tucker <tuckerwray84@gmail.com>
2020-07-24 13:31:08 -0400

    defines two NavLinks using Recat-router-Dom library modules/n - Home\n &/n - Help

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
   return (
-    <img
-      src="../../Assets/Pizza.jpg"
-      alt="A large pizza with thin crust and all the toppings!"
-    />
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/help">Help</NavLink>
+    </div>
   );
 };
