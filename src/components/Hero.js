1f08a3a30a0837ac6c3c12ee98988aff2828e9a5
tucker <tuckerwray84@gmail.com>
2020-07-25 16:20:02 -0400

    straightens out the logic for HERO file Link. linkn text and path update to allow hopping between homepage and form.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,30 +1,31 @@
-import React from "react";
-import { Link,useLocation } from "react-router-dom";
-import pizzaImg from '../Assets/Pizza.jpg'
-
-
+import React, { useState, useEffect, from } from "react";
+import { Link, useLocation } from "react-router-dom";
+import pizzaImg from "../Assets/Pizza.jpg";
+import { UserStatus } from "../constraints/user-status";
 export const Hero = () => {
+  const [path, setPath] = useState({ linkPath: "", linkText: "Pizza?" });
+
+  let location = useLocation();
+
+  useEffect(() => {
+    location.path == "/"
+      ? setPath({ linkPath: "/pizza", linkText: "PIZZA!🍕" })
+      : location.path == "/pizza" &&
+        setPath({ linkPath: "/", linkText: "HOME🏡" });
+  }, [location.path]);
+
   return (
-      <div>
-  
+    <div>
+      <Link to={`/${path.linkPath}`}>hero path.linkText==>[[{path.linkText}]]</Link>
     </div>
   );
 };
 // import React from "react";
 
 // export const Hero = () => {
-//     let location = useLocation();    
 //     let link;
 //       return (
 //         <h2>
 //           {location.pathname == "/pizza" ? location.pathname + "time" : "home"}
 //   );
 // };
-
-
-
-
-
-
-
-
